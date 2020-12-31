8a9df0b26817fe4da4340f963e14e3ac27883710
ExAl1999 <aminonimisi2@gmail.com>
2020-12-31 23:00:33 +0100

    api done remainz one

---
+++ src/key_config.js
@@ -0,0 +1,13 @@
+const D_API_KEY = {
+    'api-key': `9a08723f-96b3-4690-8d54-819a04d1c617`
+}
+
+const T_API_KEY = {
+    'X-RapidAPI-Key': `18fe9a84d3mshffbf87e112d65e3p1ac58fjsnf1ed3dd7ebe8`
+}
+
+const DZ_API_KEY = {
+    'x-rapidapi-key': `18fe9a84d3mshffbf87e112d65e3p1ac58fjsnf1ed3dd7ebe8`
+}
+
+export {D_API_KEY, T_API_KEY, DZ_API_KEY}; 
